5e429fc82a7383f2de598c76db78ad6193d7f4b0
jaewooklim1 <jaewooklim1@gmail.com>
2021-05-04 16:07:51 -0400

    almost done with phase 2

--- skeleton_2/frontend/pokedex.jsx
+++ skeleton_2/frontend/pokedex.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-
+import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions'
+import { fetchAllPokemon } from './util/api_util'
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    // const store = configureStore();
+    const store = configureStore();
+    window.receiveAllPokemon = receiveAllPokemon;
+    window.requestAllPokemon = requestAllPokemon;
+    window.fetchAllPokemon = fetchAllPokemon;
+    window.getState = store.getState; 
+    window.dispatch = store.dispatch;
+    window.store = store;
     ReactDOM.render(<h1>Pokedex</h1> ,root)
 })
